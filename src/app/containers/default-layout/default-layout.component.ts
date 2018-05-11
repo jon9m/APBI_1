c8e99a0e59f4f1956aeb7974244dc39cdeadf59a
jon <manojam79@gmail.com>
2018-05-11 17:46:03 +1000

    Server status

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,16 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
 import { navItems } from './../../_nav';
+import { HTTPService } from "../../shared/http.service";
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import 'rxjs/add/operator/takeWhile';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html'
 })
-export class DefaultLayoutComponent {
+export class DefaultLayoutComponent implements OnInit, OnDestroy {
+
+  @ViewChild('serverStatusElem') serverStatusElem: ElementRef;
+
+  private alive: boolean;
+  private interval: number;
+
   public navItems = navItems;
   public sidebarMinimized = true;
   private changes: MutationObserver;
   public element: HTMLElement = document.body;
-  constructor() {
+  constructor(private httpService: HTTPService, private renderer: Renderer2) {
+    this.alive = true;
+    this.interval = 5000;
 
     this.changes = new MutationObserver((mutations) => {
       this.sidebarMinimized = document.body.classList.contains('sidebar-minimized')
@@ -19,5 +31,30 @@ export class DefaultLayoutComponent {
     this.changes.observe(<Element>this.element, {
       attributes: true
     });
+  }
+
+  private subscription: Subscription;
+
+  ngOnInit() {
+    let statusElem = this.serverStatusElem.nativeElement;
+    let timer = TimerObservable.create(0, this.interval);
+    this.subscription = timer.takeWhile(() => this.alive).subscribe(() => {
+        this.httpService.getAppStatus().subscribe(
+          (response: Response) => {
+            this.renderer.removeClass(statusElem, 'alert-danger');
+            this.renderer.addClass(statusElem, 'alert-success');
+            statusElem.textContent = "Successful connection to the server!";
+          },
+          (error) => {
+            this.renderer.removeClass(statusElem, 'alert-success');
+            this.renderer.addClass(statusElem, 'alert-danger');
+            statusElem.textContent = "Unable to connect to the server! please check your network connection.";
+          });
+      });
+  }
+
+  ngOnDestroy() {
+    this.alive = false; // switches your TimerObservable off
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class HTTPService {
+
+    // appStatusUrl = 'https://apbi.com.au/inspector-app/cpLiveApp'; //TODO
+    appStatusUrl = 'http://localhost/static/json.json'; //TODO
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getAppStatus() {
+        let currURL = this.appStatusUrl + "?time=" + new Date().getTime();
+        return this.http.get(currURL);
+    }
+
 
 }
