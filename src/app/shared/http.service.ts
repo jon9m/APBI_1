3c3948c96fb4c112214d69860733209223347b2f
jon <manojam79@gmail.com>
2018-05-12 13:14:25 +1000

    Calendar load

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -6,6 +6,8 @@ export class HTTPService {
 
     // appStatusUrl = 'https://apbi.com.au/inspector-app/cpLiveApp'; //TODO
     appStatusUrl = 'http://localhost/static/json.json'; //TODO
+    // calendarFeedUrl = 'https://apbi.com.au/inspector-app/cpBookingCalendarFeedApp';
+    calendarFeedUrl = 'http://localhost/static/cpBookingCalendarFeedApp.json';
 
     constructor(private http: HttpClient) {
 
@@ -16,5 +18,14 @@ export class HTTPService {
         return this.http.get(currURL);
     }
 
+    loadCalendar(postObj) {
+        let currURL = this.calendarFeedUrl + "?time=" + new Date().getTime();
+        // return this.http.post(currURL, postObj);
+        return this.http.get(currURL);
+    }
 
+    getEvents() {
+        let currURL = this.calendarFeedUrl + "?time=" + new Date().getTime();
+        return this.http.get(currURL);
+    }
 }
