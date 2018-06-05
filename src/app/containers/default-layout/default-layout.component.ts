b28cda6e33a3486b316e49130c501c192e1c5ee3
Vu Tuan <vutuan@Vus-iMac.local>
2018-06-05 16:50:46 +1000

    Styles overrides

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -8,7 +8,8 @@ import { AppServeiceLoadStatusService } from "../../shared/app-service-load-stat
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './default-layout.component.html'
+  templateUrl: './default-layout.component.html',
+  styleUrls: ['./default-layout.component.scss']
 })
 export class DefaultLayoutComponent implements OnInit, OnDestroy, AfterViewInit {
 
